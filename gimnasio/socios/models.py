b82f4560c603848e55704ad8591d9d527dd18ce7
rhernandezmy <rhernandezmy@fpvirtualaragon.es>
2026-02-15 16:48:37 +0100

    Añadir cupo máximo en las clases y crear readme.md

--- gimnasio/socios/models.py
+++ gimnasio/socios/models.py
@@ -26,12 +26,16 @@ class Clase(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True, null=True)
     horario = models.DateTimeField()
+    cupo_maximo = models.PositiveIntegerField(default=20)
     
     # Un entrenador puede dar muchas clases (ForeignKey)
     entrenador = models.ForeignKey(Entrenador, on_delete=models.CASCADE, related_name='clases')
     
     # Una clase tiene muchos socios y un socio puede ir a muchas clases (ManyToManyField)
     socios = models.ManyToManyField(Socio, related_name='clases_inscritas', blank=True)
-    
+
     def __str__(self):
         return f"{self.nombre} - {self.horario}"
+    
+    def cupo_disponible(self):
+        return self.cupo_maximo - self.socios.count()
