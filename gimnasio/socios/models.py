2511bdbfb079f759fed26b30e4647fa03093d1be
rhernandezmy <rhernandezmy@fpvirtualaragon.es>
2026-02-15 13:07:14 +0100

    Creacion app gimnasio

---
+++ gimnasio/socios/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Socio(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    telefono = models.CharField(max_length=20, blank=True, null=True)
+    fecha_nacimiento = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
